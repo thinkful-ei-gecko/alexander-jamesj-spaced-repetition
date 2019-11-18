41867a1d81ef6859448932360f4c44446ccb1671
Alexander Fukui <alexander.fukui@gmail.com>
2019-11-18 10:19:03 -0600

    font choice and basic css init

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { UserProvider } from './contexts/UserContext'
 import App from './components/App/App'
 import './setup-icons'
 import './index.css'
+import './_variables.css';
 import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(
