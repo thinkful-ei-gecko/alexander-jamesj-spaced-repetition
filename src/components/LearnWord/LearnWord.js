3ac206be54bd087bdd2813ea54b4a3d215203c3e
psychicbologna <alexander.fukui@gmail.com>
2019-11-19 13:02:10 -0600

    changes to fix cypress tests

--- src/components/LearnWord/LearnWord.js
+++ src/components/LearnWord/LearnWord.js
@@ -13,11 +13,11 @@ class LearnWord extends Component {
     const { currentScore, word, correct, incorrect } = this.props
     return (
       <section className="LearnWord">
-        <h2 className="LearnWord__word">{word}</h2>
-        <p className="LearnWord__correct">Correct: {correct}</p>
+        <h4 className="LearnWord__current">Your Current Score: {currentScore}</h4>
+        <h2 className="LearnWord__word">Translate the word</h2>
+        <p className="LearnWord__word">{word}</p>
+        <p className="LearnWord__correct">Correct attempts: {correct}</p>
         <p className="LearnWord__incorrect">Incorrect: {incorrect}</p>
-
-        <h3 className="LearnWord__current">Your Current Score: {currentScore}</h3>
       </section>
     )
   }
