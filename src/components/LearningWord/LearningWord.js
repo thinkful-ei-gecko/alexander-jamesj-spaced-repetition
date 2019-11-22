c67467ea973942f61fa367e7283cad2005421774
jamesjenkinsjr <jamesjenkinsjr@icloud.com>
2019-11-22 14:09:25 -0500

    tinker with colors on correct and incorrect for LearningWord

--- src/components/LearningWord/LearningWord.js
+++ src/components/LearningWord/LearningWord.js
@@ -3,22 +3,35 @@ import { nullNextWord } from '../../contexts/LearningContext'
 import './LearningWord.css'
 import { parseForeign } from '../../utils'
 
-
 class LearningWord extends Component {
   static defaultProps = {
-    nextWord: nullNextWord
+    nextWord: nullNextWord,
   }
   render() {
     const { nextWord, langID } = this.props
     return (
       <section className="LearningWord">
-        <h2 className="LearningWord__word_heading">Translate the word/phrase:</h2>
-        <span className="LearningWord__word">{parseForeign(nextWord.word, langID)}</span>
-        <p className="LearningWord__correct">You have answered this word correctly {nextWord.correct} times.</p>
-        <p className="LearningWord__incorrect">You have answered this word incorrectly {nextWord.incorrect} times.</p>
+        <h2 className="LearningWord__word_heading">
+          Translate the word/phrase:
+        </h2>
+        <span className="LearningWord__word">
+          {parseForeign(nextWord.word, langID)}
+        </span>
+        <p className="LearningWord__correct">
+          You have answered this word correctly{' '}
+          <span className="LearningWord__correct-num">{nextWord.correct}</span>{' '}
+          times.
+        </p>
+        <p className="LearningWord__incorrect">
+          You have answered this word incorrectly{' '}
+          <span className="LearningWord__incorrect-num">
+            {nextWord.incorrect}
+          </span>{' '}
+          times.
+        </p>
       </section>
     )
   }
 }
 
-export default LearningWord;
+export default LearningWord
